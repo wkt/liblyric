8db8faf0b7af6b8e076e4937474b8a8e3070c5c0
weiketing <weikting@gmail.com>
2011-07-21 21:41:03 +0800

    modified:   liblyric/LyricShow.c

--- liblyric/LyricShow.c
+++ liblyric/LyricShow.c
@@ -26,7 +26,7 @@ lyric_show_get_type (void)
 		};
 		
 		type = g_type_register_static (G_TYPE_INTERFACE, 
-					       "NautilusView",
+					       "LyricShowView",
 					       &info, 0);
 		g_type_interface_add_prerequisite (type, G_TYPE_OBJECT);
 	}
