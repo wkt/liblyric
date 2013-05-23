deb838f7654fefdeae3b83cd688001b8049f32d5
wkt <weikting@gmail.com>
2013-05-23 13:54:39 +0800

    modified:   COPYING 	modified:   autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -49,7 +49,10 @@ fi
 	DIE=1
 }
 
-if automake-1.11 --version < /dev/null > /dev/null 2>&1 ; then
+if automake-1.12 --version < /dev/null > /dev/null 2>&1 ; then
+    AUTOMAKE=automake-1.12
+    ACLOCAL=aclocal-1.12
+else if automake-1.11 --version < /dev/null > /dev/null 2>&1 ; then
     AUTOMAKE=automake-1.11
     ACLOCAL=aclocal-1.11
 else if automake-1.10 --version < /dev/null > /dev/null 2>&1 ; then
@@ -64,6 +67,7 @@ else
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	DIE=1
+fi
 fi
 fi
 fi
